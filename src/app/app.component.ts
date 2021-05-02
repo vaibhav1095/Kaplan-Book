580f2ebada9b1eec62b4d00185bed3ee3e3a6e3d
Vaibhav Wadekar <vaib101295@gmail.com>
2021-05-02 09:17:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, ViewChild } from '@angular/core';
+import { HasElementRef } from '@angular/material/core/common-behaviors/color';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('true', style({ width: '56px' })),
+      state('false', style({ width: '240px' })),
+      transition('false <=> true', animate(500))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'kaplan-book';
+  closed = false;
+
+  @ViewChild('sidenav') sidenav : MatSidenav;
+
+  onSidenavToggle() {
+    this.closed = !this.closed;
+  }
 }
